d5d0abbac74a6f004912a127ec00871a0fb483dd
Okogod <godspowerokoro17@gmail.com>
2025-08-18 16:58:28 +0100

    add first mock exam screen

---
+++ src/components/mock-exam-components/mock-exam-nav/MockExamNav.tsx
@@ -0,0 +1,20 @@
+// Componenets
+import MockExamTimer from '../mock-exam-timer/MockExamTimer'
+import MockExamProgressBar from "../mock-exam-progress-bar/MockExamProgressBar";
+import MockExamScore from '../mock-exam-score/MockExamScore';
+
+
+const MockExamNav = () => {
+    return(
+        <div className="flex flex-col w-[90%] gap-[10px] pb-[10px] mt-[-30px]">
+            <div className='flex justify-between items-center'>
+                <h1 className="text-[24px] text-[#414d58] font-[700]">CIPM MOCK EXAM</h1>
+                <MockExamTimer/>
+                <MockExamScore/>
+            </div>
+            <MockExamProgressBar/>
+        </div>
+    )
+}
+
+export default MockExamNav
---
+++ src/static-data/MockExamQuestions.ts
@@ -0,0 +1,80 @@
+const mockExamQuestions = [
+    {
+        question: "Which of the following best describes the role of a Data Protection Officer (DPO) under the GDPR?",
+        options: [
+            "To enforce company disciplinary actions on employees violating privacy",
+            "To handle marketing communication consent for all customers",
+            "To monitor compliance with privacy laws and act as a liaison to data protection authorities",
+            "To maintain server infrastructure and oversee system availability"
+        ],
+        correctAnswer: "Paris"
+    },
+
+    {
+        question: "Which of the following best describes the role of a Data Protection Officer (DPO) under the GDPR?",
+        options: [
+            "To enforce company disciplinary actions on employees violating privacy",
+            "To handle marketing communication consent for all customers",
+            "To monitor compliance with privacy laws and act as a liaison to data protection authorities",
+            "To maintain server infrastructure and oversee system availability"
+        ],
+        correctAnswer: "Paris"
+    },
+
+    {
+        question: "Which planet is known as the Red Planet?",
+        options: ["Earth", "Mars", "Jupiter", "Venus"],
+        correctAnswer: "Mars"
+    },
+
+    {
+        question: "Who wrote 'Romeo and Juliet'?",
+        options: ["William Shakespeare", "Charles Dickens", "Jane Austen", "Mark Twain"],
+        correctAnswer: "William Shakespeare"
+    },
+
+    {
+        question: "What is the largest ocean on Earth?",
+        options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+        correctAnswer: "Pacific Ocean"
+    },
+
+    {
+        question: "Which element has the chemical symbol 'O'?",
+        options: ["Gold", "Oxygen", "Silver", "Iron"],
+        correctAnswer: "Oxygen"
+    },
+
+    {
+        question: "Which element has the chemical symbol 'O'?",
+        options: ["Gold", "Oxygen", "Silver", "Iron"],
+        correctAnswer: "Gold"
+    },
+
+    {
+        question: "Which element has the chemical symbol 'O'?",
+        options: ["Gold", "Oxygen", "Silver", "Iron"],
+        correctAnswer: "Silver"
+    },
+
+    {
+        question: "Which of the following best describes the role of a Data Protection Officer (DPO) under the GDPR?",
+        options: [
+            "To enforce company disciplinary actions on employees violating privacy",
+            "To handle marketing communication consent for all customers",
+            "To monitor compliance with privacy laws and act as a liaison to data protection authorities",
+            "To maintain server infrastructure and oversee system availability"
+        ],
+        correctAnswer: "Paris"
+    },
+
+    {
+        question: "Which planet is known as the Red Planet?",
+        options: ["Earth", "Mars", "Jupiter", "Venus"],
+        correctAnswer: "Mars"
+    }
+
+
+];
+
+export default mockExamQuestions;
