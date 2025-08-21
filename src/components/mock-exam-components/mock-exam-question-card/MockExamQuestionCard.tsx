42935731aca44eb4d905afbba1cacd71fc82d943
Okogod <godspowerokoro17@gmail.com>
2025-08-21 16:52:38 +0100

    add view result functionality

--- src/components/mock-exam-components/mock-exam-question-card/MockExamQuestionCard.tsx
+++ src/components/mock-exam-components/mock-exam-question-card/MockExamQuestionCard.tsx
@@ -1,101 +1,7 @@
-// import mockExamQuestions from "../../../static-data/MockExamQuestions";
-
-// // React Hooks
-// import { useState, useContext, useEffect } from "react";
-
-// // Assets
-// import Checkbox from '../../../assets/images/mock-exam-images/checkbox.png';
-// import Uncheckbox from '../../../assets/images/mock-exam-images/uncheckbox.png';
-
-// // Context Apis
-// import { MockExamContext } from "../../../context/MockExamContext";
-
-// type MockExamQuestionCardProps = {
-//     index: number | undefined
-//     func: (i: number) => void
-// }
-
-
-
-// type MockExamQuestionType = {
-//     question: string,
-//     options: string[],
-//     correctAnswer: string
-// }
-
-// const MockExamQuestionCard = ( {index, func}: MockExamQuestionCardProps ) => {
-
-//     const MockContext = useContext(MockExamContext)
-
-//     const [indexValue, setIndexValue] = useState(-1);
-
-//     const setDoneQuestions = (q: string, i: number) => {
-//         MockContext?.setDoneQuestions([...MockContext.doneQuestions, {questionIndex: i, optionChosen: q}])
-//         console.log(MockContext?.doneQuestions);
-
-//     //         setItems(prevItems => {
-//     //   const updatedItems = [...prevItems];
-//     //   updatedItems.splice(index, 0, newItem); 
-//     //   return updatedItems;
-//     // })
-//     }
-
-
-// // 
-    
-
-
-//     return (
-//         <div className="flex flex-col gap-[24px]">
-//             <h2 className="text-[20px] text-[#414d58] font-[700]">{mockExamQuestions[index || 0].question}</h2>
-//             {
-                
-//                 mockExamQuestions[index || 0 ].options.map((q, i) =>
-//                     <div onClick={() => {setIndexValue(i), func(i),setDoneQuestions(q, i)}} >
-//                         <OptionBox value={q}  indexValue={indexValue} index={i} />
-//                     </div>
-//                 )
-//             }
-//         </div>
-//     )
-// }
-
-// export default MockExamQuestionCard
-
-// type questionsType = {
-//     questionIndex: number,
-//     optionChosen: string
-// }
-
-
-// type OptionBoxProps = {
-//     value: questionsType | string,
-//     indexValue: number,
-//     index: number
-// }
-
-// export const OptionBox = ({ value, indexValue, index }: OptionBoxProps) => {
-
-//     const MockContext = useContext(MockExamContext)
-
-
-    
-
-
-//     return (
-//         <div className="flex gap-[24px] border-[1px] rounded-[10px] p-[20px] mb-10px cursor-pointer">
-//             <img src={MockContext?.doneQuestions.includes({questionIndex: index, optionChosen: value as string}) && indexValue == index ? Checkbox : Uncheckbox} className="w-[24px] h-[24px]" />
-//             <p className="text-[#414d58] text-[18px]">{value as string}</p>
-//         </div>
-//     )
-// }
-
-
-
 import mockExamQuestions from "../../../static-data/MockExamQuestions";
 
 // React Hooks
-import { useState, useContext, useEffect } from "react";
+import { useContext } from "react";
 
 // Assets
 import Checkbox from '../../../assets/images/mock-exam-images/checkbox.png';
@@ -113,12 +19,6 @@ type MockExamQuestionCardProps = {
 type QuestionsType = {
   questionIndex: number;
   optionChosen: string;
-};
-
-type MockExamQuestionType = {
-  question: string;
-  options: string[];
-  correctAnswer: string;
 };
 
 type OptionBoxProps = {
@@ -146,6 +46,7 @@ export const OptionBox = ({ value, questionIndex }: OptionBoxProps) => {
 };
 
 const MockExamQuestionCard = ({ index, func }: MockExamQuestionCardProps) => {
+
   const MockContext = useContext(MockExamContext);
 
   const setDoneQuestions = (q: QuestionsType) => {
@@ -156,9 +57,9 @@ const MockExamQuestionCard = ({ index, func }: MockExamQuestionCardProps) => {
       );
 
       if (existingIndex !== -1) {
-        updated[existingIndex] = q; // Replace existing answer
+        updated[existingIndex] = q; 
       } else {
-        updated.push(q); // Add new answer
+        updated.push(q);
       }
 
       return updated;
@@ -166,9 +67,6 @@ const MockExamQuestionCard = ({ index, func }: MockExamQuestionCardProps) => {
     console.log(MockContext?.doneQuestions);
   };
 
-  useEffect(() => {
-    console.log("Done Question: " + MockContext?.doneQuestions);
-  }, [MockContext?.doneQuestions])
 
   const questionData = mockExamQuestions[index ?? 0];
 
