3ed527d4047fbdde47068436bb7000e2128cd889
Majiyebo Esuga <majiyeboesuga8@gmail.com>
2025-08-22 17:14:45 +0100

    Style Share Nugget

--- src/components/Modal/ShareNuggetDetails.tsx
+++ src/components/Modal/ShareNuggetDetails.tsx
@@ -4,6 +4,11 @@ import { useNavigate } from "react-router-dom"
 import CopyLink from "../../assets/images/icons/dashboard-icons/copy-link.png"
 import ShareVia from "../../assets/images/icons/dashboard-icons/share-via.png"
 import CommunityImg from "../../assets/images/icons/dashboard-icons/community-icon-black.png"
+import LinkedinIcn from "../../assets/images/icons/dashboard-icons/linkedin-icon.png"
+import GmailIcon from "../../assets/images/icons/dashboard-icons/gmail-icon.png"
+import MessageIcon from "../../assets/images/icons/dashboard-icons/message-icon.png"
+import MailIcon from "../../assets/images/icons/dashboard-icons/mail-icon.png"
+
 
 export const ShareNuggetDetails = ({ setIsFormFilled }: { setIsFormFilled: React.Dispatch<React.SetStateAction<boolean>> }) => {
   const navigate = useNavigate()
@@ -21,6 +26,25 @@ export const ShareNuggetDetails = ({ setIsFormFilled }: { setIsFormFilled: React
       text: 'Community'
     },
   ]
+
+  const socialMedia = [
+    {
+      img: LinkedinIcn,
+      text: 'Linkedin'
+    },
+    {
+      img: GmailIcon,
+      text: 'Gmail'
+    },
+    {
+      img: MessageIcon,
+      text: 'Message'
+    },
+    {
+      img: MailIcon,
+      text: 'Mail'
+    }
+  ]
   return (
     <div className="flex flex-col">
       <div className="flex justify-between self-end w-[75%] mb-12">
@@ -30,18 +54,29 @@ export const ShareNuggetDetails = ({ setIsFormFilled }: { setIsFormFilled: React
           alt="" onClick={() => { setIsFormFilled(false), navigate('/dashboard') }} />
 
       </div>
-      <div className='align-middle justify-between flex rounded-xl'>
+      <div className='align-middle justify-between flex rounded-xl mb-6'>
         {
           shareIcons.map((share) =>
-            <div>
-              <img src={share.img} alt="" className='justify-self-center w-[48px] h-[48px] mb-10' />
-              <p className="text-slate-gray font-normal text-[18px]">{share.text}</p>
+            <div className="w-[72px] h-[80px] flex flex-col mr-2 ">
+              <img src={share.img} alt="" className='justify-self-center self-center w-[48px] h-[48px] ' />
+              <p className="text-slate-gray text-center font-normal w-[78px] h-[28px] justify-self-center  text-[18px]/7 self-center flex">{share.text}</p>
             </div>
           )
         }
-
-
       </div>
+      <div className="align-middle justify-between flex rounded-xl">
+        {
+          socialMedia.map((share) =>
+            <div className="w-[72px] h-[80px] flex flex-col ">
+              <img src={share.img} alt="" className='justify-self-center self-center w-[48px] h-[48px] ' />
+              <p className="text-slate-gray text-center font-normal text-[18px] self-center flex">{share.text}</p>
+            </div>
+          )
+        }
+      </div>
+
+
+
     </div>
   )
 }
