7551598c42237fa93f8d913102b574bb12888dd1
Paul-Esuga <majiyeboesuga8@gmail.com>
2025-12-20 08:03:24 +0100

    edit module video background

--- src/pages/modules/ModuleVideo.tsx
+++ src/pages/modules/ModuleVideo.tsx
@@ -4,7 +4,7 @@ function ModuleVideo() {
   return (
     <div className='z-[30000]  overflow-y-scroll h-[70vh] pb-10'>
       <h1>Video Files</h1>
-      <div className='p-2.5 bg-white rounded-[10px]'>
+      <div className='p-2.5 bg-transparent rounded-[10px]'>
         {/* <audio controls src={audioFile} className="w-full bg-white" ></audio> */}
         <video src={videoFile} controls color="red"></video>
       </div>
