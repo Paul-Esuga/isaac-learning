0d19d2005e4091b035505064aa4b1de8e3964094
Paul-Esuga <majiyeboesuga8@gmail.com>
2025-12-29 19:39:39 +0100

    move export default to bottom of page

--- src/pages/createaccount/UseeffectLearn.tsx
+++ src/pages/createaccount/UseeffectLearn.tsx
@@ -1,50 +1,44 @@
+import { useEffect, useState } from "react";
 
+function UseeffectLearn() {
+  // const arr = [
+  //     "/building-bg.png",
+  //     "/climbing-bg.png",
+  //     "/Learning-bg.png"
+  // ];
+  // const write = ["Testing", "Another Test", "Final Test"];
 
-import { useEffect, useState } from 'react';
+  const mutAr = {
+    pictures: ["/Learning-bg.png", "/building-bg.png", "/climbing-bg.png"],
+    write: ["Testing", "Another Test", "Final Test"],
+  };
 
-export default function UseeffectLearn() {
-    // const arr = [
-    //     "/building-bg.png",
-    //     "/climbing-bg.png",
-    //     "/Learning-bg.png"
-    // ];
-    // const write = ["Testing", "Another Test", "Final Test"];
+  const [indexval, setIndexval] = useState(0);
 
-    const mutAr = {
-        "pictures" :[
-            "/Learning-bg.png",
-            "/building-bg.png",
-            "/climbing-bg.png",
-            
-        ],
-        "write": [
-            "Testing",
-            "Another Test",
-            "Final Test"
-        ]
-    }
-    
-    const [indexval, setIndexval] = useState(0);
-    
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndexval((prev) => (prev + 1) % mutAr.pictures.length);
+    }, 3000);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setIndexval(prev => (prev + 1) % mutAr.pictures.length);
-        }, 3000);
+    return () => clearInterval(interval); // cleanup
+  }, []); // run once when component mounts
 
-        return () => clearInterval(interval); // cleanup
-    }, []); // run once when component mounts
-
-    return (
-        <div>
-            <img src={mutAr.pictures[indexval]} alt="Learning" />
-            <p>{mutAr.write[indexval]}</p>
-            <div className="flex space-x-2 mt-8">
-                {mutAr.write.map((j, i) => (
-                    <div key={i} className={`w-3 h-3 rounded-full border-1 border-red-500 ${indexval === i && 'bg-red-500'}`}/>
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <img src={mutAr.pictures[indexval]} alt="Learning" />
+      <p>{mutAr.write[indexval]}</p>
+      <div className="flex space-x-2 mt-8">
+        {mutAr.write.map((j, i) => (
+          <div
+            key={i}
+            className={`w-3 h-3 rounded-full border-1 border-red-500 ${
+              indexval === i && "bg-red-500"
+            }`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
-``
+
+export default UseeffectLearn;
